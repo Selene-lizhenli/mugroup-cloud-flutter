7447e18b262ebd873541d11a9de28415d601a77c
edram <edram@qq.com>
2025-02-19 14:52:50 +0800

    chore: 禁用路由切换效果

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -4,6 +4,9 @@ import 'router.gr.dart';
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
   @override
+  RouteType get defaultRouteType => const RouteType.custom();
+
+  @override
   List<AutoRoute> get routes {
     return [
       AutoRoute(page: HomeRoute.page, path: '/', initial: true),
