37031c0e81fd975ead31852758c7abc59f0c0736
edram <edram@qq.com>
2025-02-24 11:43:52 +0800

    chore: 增加 qr_flutter 显示二维码

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,15 +1,50 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:cloud/helper/helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 @RoutePage()
-class LoginPage extends StatelessWidget {
+class LoginPage extends HookWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Text("登录页面"),
+    useEffect(() {
+      final timer = Timer.periodic(const Duration(seconds: 1), (_) {
+        // TODO: 轮询
+        logger.d("测试");
+      });
+
+      return timer.cancel;
+    }, []);
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("云链"),
+            const SizedBox(
+              height: 5,
+            ),
+            QrImageView(
+              data: 'https://cloud.mugroup.com/login?code=12323',
+              version: QrVersions.auto,
+              size: 200.0,
+              backgroundColor: Colors.white,
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              "请使用企业微信扫码登录",
+              style: TextStyle(color: Color(0xFF707070), fontSize: 10),
+            ),
+          ],
+        ),
       ),
     );
   }
