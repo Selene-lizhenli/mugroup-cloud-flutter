2f6a43ee506d53e2de15c9ade54da44737f47ad4
frh-hh <2478804720@qq.com>
2025-02-26 17:24:41 +0800

    feat: 添加 Department model

---
+++ lib/models/department.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'department.freezed.dart';
+part 'department.g.dart';
+
+@freezed
+class Department with _$Department {
+  factory Department({
+    int? id,
+    String? name,
+  }) = _Department;
+
+  factory Department.fromJson(Map<String, dynamic> json) =>
+      _$DepartmentFromJson(json);
+}
