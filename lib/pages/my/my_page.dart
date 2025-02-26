f35606623b55051fa6c3d57082e099709ec925fe
frh-hh <2478804720@qq.com>
2025-02-26 14:44:41 +0800

    feat: 完成个人页面

--- lib/pages/my/my_page.dart
+++ lib/pages/my/my_page.dart
@@ -13,20 +13,62 @@ class MyPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF6F6F6),
       body: SafeArea(
         child: Column(
           children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.all(20),
+              color: Colors.white,
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 16),
+                  Text(
+                    '姓名',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '工号: 00001',
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '部门: 信息部',
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
             const ListTile(
+              tileColor: Colors.white,
               title: Text('设置'),
-              leading: Icon(CupertinoIcons.person_alt),
+              leading: Icon(CupertinoIcons.gear_alt_fill),
               trailing: Icon(Icons.keyboard_arrow_right_rounded),
             ),
-            ElevatedButton(
-              onPressed: () async {
-                await api.post("api/logout");
-                app.logout();
-              },
-              child: const Text("退出登录"),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () async {
+                    await api.post("api/logout");
+                    app.logout();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    backgroundColor: Colors.white,
+                    elevation: 0,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  child: const Text(
+                    '退出登录',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  )),
             ),
           ],
         ),
