7a84cff1c538c76692a04e1331c9cf9ae5083c34
frh-hh <2478804720@qq.com>
2025-02-28 16:40:34 +0800

    feat: 添加 WarehouseCard 组件

--- lib/pages/selectors/select_wms_warehouse_page.dart
+++ lib/pages/selectors/select_wms_warehouse_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cloud/models/wms.dart';
+import 'package:cloud/pages/selectors/widgets/warehouse_card.dart';
 import 'package:cloud/services/wms.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -44,8 +45,7 @@ class SelectWmsWarehousePage extends HookConsumerWidget {
                 Column(
                   children: warehouses.value
                           ?.map(
-                            (warehouseItem) => Container(
-                              padding: const EdgeInsets.symmetric(vertical: 8),
+                            (warehouseItem) => WarehouseCard(
                               child: WarehouseItem(
                                 warehouse: warehouseItem,
                               ),
