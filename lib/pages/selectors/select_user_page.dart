69ef8116095ea6a8c5f5f8b0dee8b995349f1822
wixi <3212722403@qq.com>
2025-03-03 13:29:02 +0800

    feat: 初步完成用户选择界面

--- lib/pages/selectors/select_user_page.dart
+++ lib/pages/selectors/select_user_page.dart
@@ -1,12 +1,49 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cloud/models/user.dart';
+import 'package:cloud/pages/selectors/widgets/user_card.dart';
+import 'package:cloud/pages/selectors/widgets/user_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 @RoutePage()
 class SelectUserPage extends HookConsumerWidget {
   const SelectUserPage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Text('用户选择');
+    final users = useState<List<User>?>([]);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('用户选择'),
+      ),
+      body: SafeArea(
+        child: Column(children: [
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                MultiSliver(children: [
+                  Column(
+                    children: users.value
+                            ?.map((userItem) => InkWell(
+                                  child: UserCard(
+                                    child: UserItem(
+                                      user: userItem,
+                                    ),
+                                  ),
+                                  onTap: () =>
+                                      {context.router.maybePop(userItem)},
+                                ))
+                            .toList() ??
+                        [],
+                  )
+                ])
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
---
+++ lib/pages/selectors/select_wms_warehouse/widgets/user_item.dart
@@ -0,0 +1,37 @@
+import 'package:cloud/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class UserItem extends HookWidget {
+  final User user;
+
+  const UserItem({super.key, required this.user});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: 40,
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                  "${user.name} (${user.jobNumber})",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
