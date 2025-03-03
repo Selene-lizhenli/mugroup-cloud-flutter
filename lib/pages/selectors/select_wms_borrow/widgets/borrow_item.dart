72312279daf563fa8ddae10af723653986ccbfd8
frh-hh <2478804720@qq.com>
2025-03-03 15:50:27 +0800

    refactor: 优化 BorrowItem 展示

--- lib/pages/selectors/select_wms_borrow/widgets/borrow_item.dart
+++ lib/pages/selectors/select_wms_borrow/widgets/borrow_item.dart
@@ -1,6 +1,7 @@
 import 'package:cloud/models/wms.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:intl/intl.dart';
 
 class BorrowItem extends HookWidget {
   final Borrow borrow;
@@ -24,7 +25,7 @@ class BorrowItem extends HookWidget {
         Expanded(
           flex: 1,
           child: Container(
-            height: 80,
+            height: 85,
             padding: const EdgeInsets.symmetric(vertical: 8),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -38,10 +39,33 @@ class BorrowItem extends HookWidget {
                   maxLines: 1,
                 ),
                 Text(
-                  borrow.user?.name ?? "",
-                  style: const TextStyle(fontSize: 12, color: Colors.grey),
+                  style: const TextStyle(fontSize: 14),
+                  borrow.warehouse?.name ?? "",
                   overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
+                  maxLines: 1,
+                ),
+                const SizedBox(
+                  height: 2,
+                ),
+                Row(
+                  textBaseline: TextBaseline.ideographic,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  children: [
+                    Text(
+                      borrow.user?.name ?? "",
+                      style: const TextStyle(fontSize: 12, color: Colors.grey),
+                      maxLines: 1,
+                    ),
+                    Text(
+                      borrow.borrowAt != null
+                          ? DateFormat("yyyy-MM-dd HH:mm:ss")
+                              .format(borrow.borrowAt!)
+                          : "",
+                      style: const TextStyle(fontSize: 12, color: Colors.grey),
+                      maxLines: 1,
+                    ),
+                  ],
                 ),
               ],
             ),
