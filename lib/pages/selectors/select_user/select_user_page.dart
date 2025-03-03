ab4e2a1cc926a5b331930cb00d9224f6d3fe0553
wixi <3212722403@qq.com>
2025-03-03 14:21:27 +0800

    refactor: 优化路径位置

--- lib/pages/selectors/select_user/select_user_page.dart
+++ lib/pages/selectors/select_user/select_user_page.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cloud/models/user.dart';
-import 'package:cloud/pages/selectors/widgets/user_card.dart';
-import 'package:cloud/pages/selectors/widgets/user_item.dart';
+import 'package:cloud/pages/selectors/select_user/widgets/user_card.dart';
+import 'package:cloud/pages/selectors/select_user/widgets/user_item.dart';
 import 'package:cloud/services/tenant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
