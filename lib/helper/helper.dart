aaa5a4ea043cb38d068a539adc30c61285d026ab
frh-hh <2478804720@qq.com>
2025-03-04 15:12:33 +0800

    feat: 添加 isUrl 辅助方法

--- lib/helper/helper.dart
+++ lib/helper/helper.dart
@@ -1,3 +1,12 @@
 import 'package:logger/logger.dart';
 
 final logger = Logger();
+
+bool isUrl(String result) {
+  // 正则表达式匹配 HTTP/HTTPS、FTP、电子邮件、电话、短信、地理位置、WhatsApp 等协议
+  RegExp urlRegex = RegExp(
+    r'^(https?|ftp|mailto|tel|sms|geo|whatsapp):\/\/[^\s]+$',
+    caseSensitive: false,
+  );
+  return urlRegex.hasMatch(result);
+}
