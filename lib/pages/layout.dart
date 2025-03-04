9b0fb702347199385c472b55a3c8bed8707b348f
edram <edram@qq.com>
2025-03-04 14:27:39 +0800

    wip: 版本更新 UI

--- lib/pages/layout.dart
+++ lib/pages/layout.dart
@@ -1,17 +1,43 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 @RoutePage()
-class Layout extends StatefulWidget {
+class Layout extends StatefulHookConsumerWidget {
   const Layout({super.key});
 
   @override
-  State<Layout> createState() => _LayoutState();
+  ConsumerState<Layout> createState() => _LayoutState();
 }
 
-class _LayoutState extends State<Layout> {
+class _LayoutState extends ConsumerState<Layout> {
   @override
   Widget build(BuildContext context) {
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text("发现新版本"),
+              content: const Text("1. xxx\n2. xxxx"),
+              actions: [
+                TextButton(
+                  child: const Text('升级'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      });
+
+      return null;
+    }, []);
+
     return AutoTabsRouter(
       builder: (context, child) {
         return Scaffold(
