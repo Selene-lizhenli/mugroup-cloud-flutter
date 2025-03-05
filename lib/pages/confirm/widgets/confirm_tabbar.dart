b17115a41172652e673eeb732e61e7f9e75de811
wixi <3212722403@qq.com>
2025-03-05 10:52:53 +0800

    refactor: 优化ConfirmTabbar

--- lib/pages/confirm/widgets/confirm_tabbar.dart
+++ lib/pages/confirm/widgets/confirm_tabbar.dart
@@ -8,8 +8,35 @@ class ConfirmTabbar extends HookConsumerWidget {
     return Container(
       height: 60,
       color: Colors.white,
-      child: const Row(
-        children: [Text('确认')],
+      child: Row(
+        children: [
+          const Expanded(
+              child: Column(
+            children: [Text('wixi')],
+          )),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+                onPressed: () async {},
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue, // 设置按钮文字颜色
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 24.0), // 设置内边距
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0), // 设置按钮圆角
+                  ),
+                  elevation: 5, // 设置按钮的阴影效果
+                ),
+                child: const Text(
+                  '确认',
+                  style: TextStyle(
+                    fontSize: 16.0, // 设置字体大小
+                    fontWeight: FontWeight.bold, // 设置字体粗细
+                  ),
+                )),
+          )
+        ],
       ),
     );
   }
