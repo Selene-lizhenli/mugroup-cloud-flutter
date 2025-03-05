471bcdfd61ca7d0aa17031162510a3648daed600
wixi <3212722403@qq.com>
2025-03-05 10:23:52 +0800

    feat: 盘点明细页面添加图片显示

--- lib/pages/confirm/widgets/confirm_item.dart
+++ lib/pages/confirm/widgets/confirm_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud/pages/cart/cart_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -8,9 +9,26 @@ class ConfirmItem extends HookWidget {
   const ConfirmItem({super.key, this.item});
   @override
   Widget build(BuildContext context) {
+    var cover = item?.sample.image?.elementAtOrNull(0)?.thumbUrl;
+    
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        cover != null
+            ? CachedNetworkImage(
+                width: 100,
+                height: 100,
+                fit: BoxFit.contain,
+                imageUrl: cover,
+              )
+            : Container(
+                width: 100,
+                height: 100,
+                color: Colors.amber,
+              ),
+        const SizedBox(
+          width: 10,
+        ),
         Expanded(
           flex: 1,
           child: Container(
