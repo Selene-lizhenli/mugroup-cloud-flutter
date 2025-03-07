041a675cf453bcec6d1bdb34d6697e9eb453decb
frh-hh <2478804720@qq.com>
2025-03-07 17:20:13 +0800

    refactor: 重构 transfer 页面

--- lib/pages/wms/wms_transfer_page.dart
+++ lib/pages/wms/wms_transfer_page.dart
@@ -1,6 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cloud/models/wms.dart';
+import 'package:cloud/router/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'widgets/operate_bar.dart';
+import 'widgets/transfer_text_card.dart';
 
 @RoutePage()
 class WmsTransferPage extends HookConsumerWidget {
@@ -10,12 +15,73 @@ class WmsTransferPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    Map<TransferStatus, String> statusMap = {
+      TransferStatus.draft: "待生效",
+      TransferStatus.processing: "运输中",
+      TransferStatus.finished: "调拨完成",
+      TransferStatus.cancelled: "已取消",
+    };
+
+    var transfer = Transfer(
+        orderNo: "SF202503050016",
+        outWarehouse: const Warehouse(name: "仓库1"),
+        inWarehouse: const Warehouse(name: "仓库2"),
+        status: TransferStatus.processing);
+
+    void onPressed() {
+      context.pushRoute(const CartRoute());
+    }
+
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [Text("调拨单$code")],
+      appBar: AppBar(
+          title: const InkWell(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.ideographic,
+          children: [
+            Text('调拨单详情'),
+          ],
         ),
-      ),
+      )),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Expanded(
+              child: Column(
+            children: [
+              TransferTextCard(
+                title: "调拨单号",
+                lable: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+                      child: Text(
+                          style: const TextStyle(fontWeight: FontWeight.w600),
+                          transfer.orderNo ?? ""),
+                    ),
+
+                    /// TODO: 优化状态显示效果
+                    Text('(${statusMap[transfer.status]})')
+                  ],
+                ),
+              ),
+              TransferTextCard(
+                title: "出库方",
+                lable: Text(
+                    style: const TextStyle(fontWeight: FontWeight.w600),
+                    transfer.outWarehouse?.name ?? ""),
+              ),
+              TransferTextCard(
+                title: '入库方',
+                lable: Text(
+                    style: const TextStyle(fontWeight: FontWeight.w600),
+                    transfer.inWarehouse?.name ?? ""),
+              ),
+            ],
+          )),
+          OperateBar(onPressed: onPressed)
+        ],
+      )),
     );
   }
 }
--- lib/router/router.dart
+++ lib/router/router.dart
@@ -33,7 +33,6 @@ class AppRouter extends RootStackRouter {
 
       AutoRoute(page: LoginRoute.page, path: "/login"),
       AutoRoute(page: ScanRoute.page, path: "/scan"),
-      AutoRoute(page: TransferRoute.page, path: "/transfer"),
       AutoRoute(page: WmsTransferRoute.page, path: "/wms/transfer/:code"),
 
       // selectors
