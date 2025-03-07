09219567e67f6ff6387d24cf63d71c7c71f95c6d
frh-hh <2478804720@qq.com>
2025-03-07 16:40:20 +0800

    feat: 完成调拨单页面 描述信息展示

--- lib/pages/transfer/transfer_page.dart
+++ lib/pages/transfer/transfer_page.dart
@@ -1,16 +1,71 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cloud/models/wms.dart';
+import 'package:cloud/pages/transfer/widgets/operate_bar.dart';
+import 'package:cloud/router/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'widgets/transfer_text_card.dart';
 
 @RoutePage()
 class TransferPage extends HookConsumerWidget {
   const TransferPage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
+    Map<TransferStatus, String> statusMap = {
+      TransferStatus.draft: "待生效",
+      TransferStatus.processing: "运输中",
+      TransferStatus.finished: "调拨完成",
+      TransferStatus.cancelled: "已取消",
+    };
+
+    var transfer = Transfer(
+        orderNo: "SF202503050016",
+        outWarehouse: const Warehouse(name: "仓库1"),
+        inWarehouse: const Warehouse(name: "仓库2"),
+        status: TransferStatus.processing);
+
+    void onPressed() {
+      context.pushRoute(const CartRoute());
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+          title: const InkWell(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.ideographic,
+          children: [
+            Text('调拨单详情'),
+          ],
+        ),
+      )),
       body: SafeArea(
           child: Column(
-        children: [Text("调拨单")],
+        children: [
+          Expanded(
+              child: Column(
+            children: [
+              TransferTextCard(
+                title: "调拨单号",
+                lable: transfer.orderNo,
+              ),
+              TransferTextCard(
+                title: "出库方",
+                lable: transfer.outWarehouse?.name,
+              ),
+              TransferTextCard(
+                title: '入库方',
+                lable: transfer.inWarehouse?.name,
+              ),
+              TransferTextCard(
+                title: '状态',
+                lable: statusMap[transfer.status],
+              ),
+            ],
+          )),
+          OperateBar(onPressed: onPressed)
+        ],
       )),
     );
   }
---
+++ lib/pages/transfer/widgets/operate_bar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class OperateBar extends HookConsumerWidget {
+  final void Function()? onPressed;
+
+  const OperateBar({super.key, this.onPressed});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SizedBox(
+      height: 60,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(
+                onPressed: onPressed,
+                child: const Text(
+                  "确认",
+                )),
+            TextButton(
+                onPressed: onPressed,
+                child: const Text(
+                  "确认",
+                )),
+            TextButton(
+                onPressed: onPressed,
+                child: const Text(
+                  "确认",
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
