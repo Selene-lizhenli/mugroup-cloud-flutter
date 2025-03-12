873c25226c46b4efd3abda7cf689e4ebac72a71d
edram <edram@qq.com>
2025-03-12 20:57:12 +0800

    chore: 增加 shared_preferences 处理持久化数据

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,14 +5,18 @@ import 'package:cloud/providers/app_provider.dart';
 import 'package:cloud/services/tenant.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class App {
   late final Directory temporaryDirectory;
+
+  late final SharedPreferences prefs;
 
   final container = ProviderContainer();
 
   Future bootstrap() async {
     temporaryDirectory = await getTemporaryDirectory();
+    prefs = await SharedPreferences.getInstance();
   }
 
   Future<User?> fetchUser() async {
