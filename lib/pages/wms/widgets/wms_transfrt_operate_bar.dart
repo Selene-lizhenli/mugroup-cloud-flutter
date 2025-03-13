def321b9d3efc2593d1c85a1a1b7ee4cba8e79db
frh-hh <2478804720@qq.com>
2025-03-13 11:23:18 +0800

    refactor: 优化 WmsTransferOperateBar

--- lib/pages/wms/widgets/wms_transfrt_operate_bar.dart
+++ lib/pages/wms/widgets/wms_transfrt_operate_bar.dart
@@ -27,45 +27,77 @@ class WmsTransferOperateBar extends HookConsumerWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              child: TextButton(
-                onPressed: status == TransferStatus.draft
-                    ? addTransferItems
-                    : transferIn,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.green[700], // 设置背景颜色
-                  foregroundColor: Colors.white, // 设置文字颜色
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16, vertical: 16), // 设置内边距
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8), // 圆角
-                  ),
-                ),
-                child: Text(
-                  status == TransferStatus.draft ? '添加' : '入库',
-                ),
+            if (status == TransferStatus.draft)
+              Expanded(
+                child: TextButton(
+                    onPressed: addTransferItems,
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.green[700], // 设置背景颜色
+                      foregroundColor: Colors.white, // 设置文字颜色
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 16), // 设置内边距
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8), // 圆角
+                      ),
+                    ),
+                    child: const Text(
+                      "添加",
+                    )),
               ),
-            ),
-            const SizedBox(width: 8),
-            Expanded(
-              child: TextButton(
-                onPressed: status == TransferStatus.draft
-                    ? transferOut
-                    : transferInAll,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue[700], // 设置背景颜色
-                  foregroundColor: Colors.white, // 设置文字颜色
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16, vertical: 16), // 设置内边距
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8), // 圆角
-                  ),
-                ),
-                child: Text(
-                  status == TransferStatus.draft ? '确认出库' : '整箱入库',
-                ),
+            if (status == TransferStatus.processing)
+              Expanded(
+                child: TextButton(
+                    onPressed: transferIn,
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.green[700], // 设置背景颜色
+                      foregroundColor: Colors.white, // 设置文字颜色
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 16), // 设置内边距
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8), // 圆角
+                      ),
+                    ),
+                    child: const Text(
+                      "入库",
+                    )),
               ),
+            const SizedBox(
+              width: 8,
             ),
+            if (status == TransferStatus.draft)
+              Expanded(
+                child: TextButton(
+                    onPressed: transferOut,
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue[700], // 设置背景颜色
+                      foregroundColor: Colors.white, // 设置文字颜色
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 16), // 设置内边距
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8), // 圆角
+                      ),
+                    ),
+                    child: const Text(
+                      "确认出库",
+                    )),
+              ),
+            if (status == TransferStatus.processing)
+              Expanded(
+                child: TextButton(
+                    onPressed: transferInAll,
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue[700], // 设置背景颜色
+                      foregroundColor: Colors.white, // 设置文字颜色
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 16), // 设置内边距
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8), // 圆角
+                      ),
+                    ),
+                    child: const Text(
+                      "整箱入库",
+                    )),
+              ),
           ],
         ),
       ),
