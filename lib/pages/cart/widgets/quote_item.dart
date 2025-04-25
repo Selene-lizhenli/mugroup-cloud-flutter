f7ed75f143623d57df051cbc9cfd14b55bab1ecd
frh-hh <2478804720@qq.com>
2025-04-25 13:38:45 +0800

    feat: 初步完成 QuoteItem

---
+++ lib/pages/cart/widgets/quote_item.dart
@@ -0,0 +1,43 @@
+import 'package:cloud/models/supply/quote.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:intl/intl.dart';
+
+class QuoteItem extends HookWidget {
+  final Quote? item;
+
+  const QuoteItem({
+    super.key,
+    this.item,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('地区: ${item?.sampleLocation}'),
+              Text('体积: ${item?.outerVolume}'),
+              Text('包装: ${item?.packing}'),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('出货日期: ${DateFormat('yyyy-MM-dd').format(item!.chuhuoAt!)}'),
+              Text('装箱量: ${item?.outerCapacity}'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
