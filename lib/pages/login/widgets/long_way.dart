d21918c7d075120ca598c7ee6aa69a295b38b60d
edram <edram@qq.com>
2025-06-20 21:01:43 +0800

    wip: 重构扫码登录

--- lib/pages/login/widgets/long_way.dart
+++ lib/pages/login/widgets/long_way.dart
@@ -1,6 +1,8 @@
+import 'package:cloud/pages/login/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class LoginWay extends HookConsumerWidget {
   final String loginWay;
@@ -8,13 +10,7 @@ class LoginWay extends HookConsumerWidget {
   const LoginWay({super.key, required this.loginWay});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final title = useMemoized(() {
-      if (loginWay == "account") {
-        return "账号密码登录";
-      }
-
-      return "";
-    }, [loginWay]);
+    final qrcodeLoading = useState(false);
 
     return Container(
       width: double.infinity,
@@ -24,7 +20,7 @@ class LoginWay extends HookConsumerWidget {
         children: [
           Text(
             style: const TextStyle(fontWeight: FontWeight.w700),
-            title,
+            getLableByLoginWay(loginWay),
           ),
 
           // 账号密码
@@ -34,7 +30,7 @@ class LoginWay extends HookConsumerWidget {
             ),
             TextField(
               style: const TextStyle(
-                fontSize: 12, // 设置文字大小
+                fontSize: 12,
               ),
               decoration: InputDecoration(
                 hintText: '账号',
@@ -48,13 +44,13 @@ class LoginWay extends HookConsumerWidget {
                   borderSide: BorderSide(
                     color: Colors.grey.withOpacity(0.5),
                     width: 0.3,
-                  ), // 聚焦时同样线宽
+                  ),
                 ),
               ),
             ),
             TextField(
               style: const TextStyle(
-                fontSize: 12, // 设置文字大小
+                fontSize: 12,
               ),
               decoration: InputDecoration(
                 hintText: '密码',
@@ -68,14 +64,51 @@ class LoginWay extends HookConsumerWidget {
                   borderSide: BorderSide(
                     color: Colors.grey.withOpacity(0.5),
                     width: 0.3,
-                  ), // 聚焦时同样线宽
+                  ),
                 ),
               ),
             ),
           ],
 
           // TODO: 企业扫码登录
-          if (loginWay == "wxwork") ...[const Text("123")],
+          if (loginWay == "wxwork") ...[
+            Center(
+              child: Column(
+                children: [
+                  if (qrcodeLoading.value)
+                    const SizedBox(
+                      width: 220,
+                      height: 220,
+                      child: Center(
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 3,
+                          ),
+                        ),
+                      ),
+                    )
+                  else
+                    Column(
+                      children: [
+                        QrImageView(
+                          data: '12123',
+                          version: QrVersions.auto,
+                          size: 220.0,
+                          backgroundColor: Colors.white,
+                        ),
+                        const Text(
+                          "请使用企业微信扫码登录",
+                          style:
+                              TextStyle(color: Color(0xFF707070), fontSize: 14),
+                        )
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          ],
 
           if (['account'].contains(loginWay))
             Container(
