5d8693f30d2a726800981b76cb5897190618fabc
edram <edram@qq.com>
2025-10-24 12:01:13 +0800

    refactor: 优化购物车底部操作栏

--- lib/pages/cart/widgets/operate_bar.dart
+++ lib/pages/cart/widgets/operate_bar.dart
@@ -47,64 +47,65 @@ class OperateBar extends HookConsumerWidget {
       child: Row(
         children: [
           Expanded(
-              child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.ideographic,
                     children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        textBaseline: TextBaseline.ideographic,
-                        children: [
-                          Text("已选: $totalCount 件"),
-                          const SizedBox(
-                            width: 1,
-                          ),
-                          if (cartType != CartType.quotation)
-                            if (showPrice)
-                              Row(
-                                children: [
-                                  const Text("合计: "),
-                                  const Text(
-                                      style: TextStyle(
-                                          color: Colors.red,
-                                          fontWeight: FontWeight.w400),
-                                      '¥'),
-                                  Text(
-                                      style: const TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.red,
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                      totalPrice.toStringAsFixed(2)),
-                                ],
-                              ),
-                        ],
+                      Text("已选: $totalCount 件"),
+                      const SizedBox(
+                        width: 1,
                       ),
+                      if (cartType != CartType.quotation)
+                        if (showPrice)
+                          Row(
+                            children: [
+                              const Text("合计: "),
+                              const Text(
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.w400),
+                                  '¥'),
+                              Text(
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                  totalPrice.toStringAsFixed(2)),
+                            ],
+                          ),
                     ],
-                  ))),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextButton(
-                onPressed: onPressed,
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.blue, // 设置按钮文字颜色
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 12.0, horizontal: 24.0), // 设置内边距
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0), // 设置按钮圆角
                   ),
-                  elevation: 5, // 设置按钮的阴影效果
-                ),
-                child: Text(
-                  buttonText[cartType] ?? "确认",
-                  style: const TextStyle(
-                    fontSize: 16.0, // 设置字体大小
-                    fontWeight: FontWeight.bold, // 设置字体粗细
-                  ),
-                )),
+                ],
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: onPressed,
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.blue,
+              padding: const EdgeInsets.symmetric(), // 设置内边距
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              elevation: 5,
+            ),
+            child: Text(
+              buttonText[cartType] ?? "确认",
+              style: const TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
           )
         ],
       ),
