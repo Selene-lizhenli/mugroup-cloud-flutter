352e0ac36d6b37dd53d60a95f8b1fd8abbca0720
edram <edram@qq.com>
2025-11-24 13:09:11 +0800

    refactor: 调整样品页面

--- lib/pages/showroom/showroom_sample_detail_page.dart
+++ lib/pages/showroom/showroom_sample_detail_page.dart
@@ -30,193 +30,187 @@ class ShowroomSampleDetailPage extends HookConsumerWidget {
       return () {};
     }, [id]);
 
+    if (sample.value == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('样品详情'),
-        backgroundColor: colorScheme.secondary,
-        elevation: 0,
-      ),
-      body: sample.value == null
-          ? const Center(child: CircularProgressIndicator())
-          : CustomScrollView(
-              slivers: [
-                // 图片轮播区域
-                if (sample.value?.image != null &&
-                    sample.value!.image!.isNotEmpty)
-                  SliverToBoxAdapter(
-                    child: Stack(
-                      children: [
-                        CarouselSlider(
-                          items: sample.value!.image!.map((item) {
-                            return Builder(
-                              builder: (BuildContext context) {
-                                return LayoutBuilder(
-                                  builder: (context, constraints) {
-                                    double containerWidth =
-                                        constraints.maxWidth;
-                                    return ClipRRect(
-                                      child: Image.network(
-                                        item.url!,
-                                        fit: BoxFit.fill,
-                                        width: containerWidth, // 确保图片显示完整，不裁剪
-                                      ),
-                                    );
-                                  },
-                                );
-                              },
-                            );
-                          }).toList(),
-                          options: CarouselOptions(
-                            viewportFraction: 1.0, // 图片占满屏幕
-                            height: 350,
-                            onPageChanged: (index, reason) {
-                              currentIndex.value = index;
+      body: CustomScrollView(
+        slivers: [
+          // 图片轮播区域
+          if (sample.value?.image != null && sample.value!.image!.isNotEmpty)
+            SliverToBoxAdapter(
+              child: Stack(
+                children: [
+                  CarouselSlider(
+                    items: sample.value!.image!.map((item) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return LayoutBuilder(
+                            builder: (context, constraints) {
+                              double containerWidth = constraints.maxWidth;
+                              return ClipRRect(
+                                child: Image.network(
+                                  item.url!,
+                                  fit: BoxFit.fill,
+                                  width: containerWidth, // 确保图片显示完整，不裁剪
+                                ),
+                              );
                             },
-                          ),
-                        ),
-                        // 右下角的图片索引
-                        Positioned(
-                          bottom: 8.0,
-                          right: 8.0,
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0, vertical: 4.0),
-                            decoration: BoxDecoration(
-                              color: Colors.black45,
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                            child: Text(
-                              '${currentIndex.value + 1} / ${sample.value!.image!.length}',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                          );
+                        },
+                      );
+                    }).toList(),
+                    options: CarouselOptions(
+                      viewportFraction: 1.0, // 图片占满屏幕
+                      height: 350,
+                      onPageChanged: (index, reason) {
+                        currentIndex.value = index;
+                      },
                     ),
                   ),
-                // 产品名称区域
-                SliverToBoxAdapter(
-                  child: Container(
-                    color: Colors.white,
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 12.0, vertical: 6),
-                    child: Text(
-                      sample.value!.nameCn ?? '',
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
+                  // 右下角的图片索引
+                  Positioned(
+                    bottom: 8.0,
+                    right: 8.0,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 4.0),
+                      decoration: BoxDecoration(
+                        color: Colors.black45,
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
+                      child: Text(
+                        '${currentIndex.value + 1} / ${sample.value!.image!.length}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                // 产品编号和价格信息
-                SliverToBoxAdapter(
-                  child: Container(
-                    color: Colors.white,
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 12.0, vertical: 4),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '产品编号: ${sample.value!.productNo}',
-                          style:
-                              const TextStyle(fontSize: 16, color: Colors.grey),
-                        ),
-                        Text(
-                          '¥${(double.tryParse(sample.value!.purchaseCost.toString()) ?? 0.0).toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: colorScheme.secondary,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SliverToBoxAdapter(child: SizedBox(height: 12)), // 添加间隔
-                // 其他信息区域
-                SliverToBoxAdapter(
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 16.0),
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '规格: ${sample.value!.spec}',
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black87,
-                          ),
-                        ),
-                        const SizedBox(height: 12),
-                        Text(
-                          '分类: ${sample.value!.category?.name ?? '未知'}',
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black87,
-                          ),
-                        ),
-                        const SizedBox(height: 12),
-                        Text(
-                          '条形码: ${sample.value!.barcode ?? '未知'}',
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black87,
-                          ),
-                        ),
-                        const SizedBox(height: 12),
-                        Text(
-                          '税率: ${sample.value!.taxRate?.toString() ?? '未知'}',
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black87,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SliverToBoxAdapter(child: SizedBox(height: 12)),
-                SliverGrid(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      final quote = sample.value?.supplyQuotes?[index];
-                      logger.d('wixi');
-                      if (quote == null) {
-                        return const SizedBox(); // 处理空的报价
-                      }
-                      return SupplyQuoteCard(quote: quote);
-                    },
-                    childCount: sample.value?.supplyQuotes?.length ?? 0,
-                  ),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 5,
-                    mainAxisSpacing: 5,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
+          // 产品名称区域
+          SliverToBoxAdapter(
+            child: Container(
+              color: Colors.white,
+              width: double.infinity,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6),
+              child: Text(
+                sample.value!.nameCn ?? '',
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+          // 产品编号和价格信息
+          SliverToBoxAdapter(
+            child: Container(
+              color: Colors.white,
+              width: double.infinity,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '产品编号: ${sample.value!.productNo}',
+                    style: const TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                  Text(
+                    '¥${(double.tryParse(sample.value!.purchaseCost.toString()) ?? 0.0).toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: colorScheme.secondary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 12)), // 添加间隔
+          // 其他信息区域
+          SliverToBoxAdapter(
+            child: Container(
+              width: double.infinity,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '规格: ${sample.value!.spec}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    '分类: ${sample.value!.category?.name ?? '未知'}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    '条形码: ${sample.value!.barcode ?? '未知'}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    '税率: ${sample.value!.taxRate?.toString() ?? '未知'}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 12)),
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final quote = sample.value?.supplyQuotes?[index];
+                logger.d('wixi');
+                if (quote == null) {
+                  return const SizedBox(); // 处理空的报价
+                }
+                return SupplyQuoteCard(quote: quote);
+              },
+              childCount: sample.value?.supplyQuotes?.length ?? 0,
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
