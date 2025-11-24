b1b0a0bffc1ebaa3a0843ff94178e471e9c144b5
wixi <3212722403@qq.com>
2025-11-24 17:28:18 +0800

    feat: 添加新建用户入口

--- lib/pages/crm/crm_company/crm_company_page.dart
+++ lib/pages/crm/crm_company/crm_company_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cloud/pages/crm/views/company_view.dart';
+import 'package:cloud/router/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -19,6 +20,14 @@ class CrmCompanyPage extends HookConsumerWidget {
             child: CompanyView(),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final shouldRefresh =
+              await context.router.push(const CrmCompanyCreateRoute());
+        },
+        tooltip: '新建客户',
+        child: const Icon(Icons.add),
       ),
     );
   }
