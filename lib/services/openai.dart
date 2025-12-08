bfa3d5ba787a82b77dc3e564230c8dc8a4be71e1
wixi <3212722403@qq.com>
2025-12-08 11:48:01 +0800

    feat: 添加供应商名片识别接口，以及样品图片识别接口

--- lib/services/openai.dart
+++ lib/services/openai.dart
@@ -1,7 +1,31 @@
 import 'package:cloud/http/api.dart';
 
+Future identifySample(Map<String, dynamic>? data) async {
+  return api.post("api/tenant/openai/identifySample", data: data).then(
+    (res) {
+      if (res.data == null) {
+        return null;
+      }
+      return Map<String, dynamic>.from(res.data);
+    },
+  );
+}
+
 Future identifyCompanyCard(Map<String, dynamic>? data) async {
   return api.post("api/tenant/openai/identifyCompanyCard", data: data).then(
+    (res) {
+      if (res.data == null) {
+        return null;
+      }
+      return Map<String, dynamic>.from(res.data);
+    },
+  );
+}
+
+Future identifySupplySuppliersCard(Map<String, dynamic>? data) async {
+  return api
+      .post("api/tenant/openai/identifySupplySuppliersCard", data: data)
+      .then(
     (res) {
       if (res.data == null) {
         return null;
