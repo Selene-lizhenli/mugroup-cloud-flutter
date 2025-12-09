4370fe4e0059ae25b0e0d40000dff29c738590ec
edram <edram@qq.com>
2025-12-09 11:24:28 +0800

    wip: 产品筛选

--- lib/pages/home/views/product_view.dart
+++ lib/pages/home/views/product_view.dart
@@ -12,6 +12,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sliver_tools/sliver_tools.dart';
+import 'package:tdesign_flutter/tdesign_flutter.dart';
 
 const pageSize = 20;
 
@@ -31,6 +33,18 @@ class ProductView extends HookConsumerWidget {
     final media = useState<TemporaryMedia?>(home.currentMedia);
     final page = useRef(1);
     final samples = useState<List<Sample>>(<Sample>[]);
+
+    final mediaQuery = MediaQuery.of(context);
+
+    var crossAxisCount = 2;
+
+    if (mediaQuery.size.width > 500) {
+      crossAxisCount = 3;
+    }
+
+    if (mediaQuery.size.width > 800) {
+      crossAxisCount = 4;
+    }
 
     fetchData(
       String? searchText, {
@@ -124,42 +138,128 @@ class ProductView extends HookConsumerWidget {
             ? IndicatorResult.success
             : IndicatorResult.noMore);
       },
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          final availableWidth = constraints.maxWidth;
+      child: CustomScrollView(
+        slivers: [
+          MultiSliver(
+            children: [
+              // 解决 Header 下拉刷新时不会跟着移动的
+              Container(
+                height: 0,
+              ),
+              const SliverPinnedHeader(
+                child: ProductDropdownMenu(),
+              ),
+              MasonryGridView.count(
+                crossAxisCount: crossAxisCount,
+                mainAxisSpacing: 5,
+                crossAxisSpacing: 5,
+                itemCount: samples.value.length,
+                padding: const EdgeInsets.all(5),
+                clipBehavior: Clip.none,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  final sample = samples.value[index];
+                  final cartItem = cartState.items.firstWhereOrNull(
+                      (element) => element.sample.id == sample.id);
 
-          var crossAxisCount = 2;
-
-          if (availableWidth > 500) {
-            crossAxisCount = 3;
-          }
-
-          if (availableWidth > 800) {
-            crossAxisCount = 4;
-          }
-
-          return MasonryGridView.count(
-            crossAxisCount: crossAxisCount,
-            mainAxisSpacing: 5,
-            crossAxisSpacing: 5,
-            itemCount: samples.value.length,
-            padding: const EdgeInsets.all(5),
-            shrinkWrap: false,
-            itemBuilder: (context, index) {
-              final sample = samples.value[index];
-              final cartItem = cartState.items.firstWhereOrNull(
-                  (element) => element.sample.id == sample.id);
-
-              return ProductCard(
-                sample: sample,
-                cartCount: cartItem?.count,
-                onTapAddSample: () {
-                  cart.addSample(sample, 1);
+                  return ProductCard(
+                    sample: sample,
+                    cartCount: cartItem?.count,
+                    onTapAddSample: () {
+                      cart.addSample(sample, 1);
+                    },
+                  );
                 },
-              );
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProductDropdownMenu extends StatelessWidget {
+  const ProductDropdownMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: TDDropdownMenu(
+        direction: TDDropdownMenuDirection.down,
+        // isScrollable: true,
+        items: [
+          TDDropdownItem(
+            label: '产品目录',
+            multiple: true,
+            options: [
+              TDDropdownItemOption(label: '选项2', value: '2'),
+              TDDropdownItemOption(label: '选项3', value: '3'),
+              TDDropdownItemOption(label: '选项4', value: '4'),
+              TDDropdownItemOption(label: '选项5', value: '5'),
+              TDDropdownItemOption(label: '选项6', value: '6'),
+              TDDropdownItemOption(label: '选项7', value: '7'),
+              TDDropdownItemOption(label: '选项8', value: '8'),
+            ],
+            onChange: (value) {
+              print('选择：$value');
             },
-          );
-        },
+            onConfirm: (value) {
+              print('确定选择：$value');
+            },
+            onReset: () {
+              print('清空选择');
+            },
+          ),
+          TDDropdownItem(
+            label: '供应商',
+            multiple: true,
+            options: [
+              TDDropdownItemOption(label: '选项2', value: '2'),
+              TDDropdownItemOption(label: '选项3', value: '3'),
+              TDDropdownItemOption(label: '选项4', value: '4'),
+              TDDropdownItemOption(label: '选项5', value: '5'),
+              TDDropdownItemOption(label: '选项6', value: '6'),
+              TDDropdownItemOption(label: '选项7', value: '7'),
+              TDDropdownItemOption(label: '选项8', value: '8'),
+            ],
+            onChange: (value) {
+              print('选择：$value');
+            },
+            onConfirm: (value) {
+              print('确定选择：$value');
+            },
+            onReset: () {
+              print('清空选择');
+            },
+          ),
+          TDDropdownItem(
+            label: '贸易国别',
+            multiple: true,
+            options: [
+              TDDropdownItemOption(label: '选项2', value: '2'),
+              TDDropdownItemOption(label: '选项3', value: '3'),
+              TDDropdownItemOption(label: '选项4', value: '4'),
+              TDDropdownItemOption(label: '选项5', value: '5'),
+              TDDropdownItemOption(label: '选项6', value: '6'),
+              TDDropdownItemOption(label: '选项7', value: '7'),
+              TDDropdownItemOption(label: '选项8', value: '8'),
+            ],
+            onChange: (value) {
+              print('选择：$value');
+            },
+            onConfirm: (value) {
+              print('确定选择：$value');
+            },
+            onReset: () {
+              print('清空选择');
+            },
+          ),
+        ],
       ),
     );
   }
