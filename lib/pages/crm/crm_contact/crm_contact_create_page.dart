8105c1cfc81d2c7add85e988b893d71f6bd27e04
wixi <3212722403@qq.com>
2025-12-12 11:57:38 +0800

    feat: 添加客户联系人页面框架

---
+++ lib/pages/crm/crm_contact/crm_contact_create_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class CrmContactCreatePage extends HookConsumerWidget {
+  const CrmContactCreatePage({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Text('客户联系人创建页面');
+  }
+}
