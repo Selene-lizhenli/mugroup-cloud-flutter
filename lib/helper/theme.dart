38928c3e2a58946c458e83ccc2314734cb2f820d
Selene <a09lili@163.com>
2026-03-04 11:30:33 +0800

    feat: 添加38节日

--- lib/helper/theme.dart
+++ lib/helper/theme.dart
@@ -8,7 +8,7 @@ final isSpringFestival = () {
   // 春节特殊时间段：
   // 2025-02-10 ~ 2025-02-26
   // 2027-01-30 ~ 2027-02-15
-  // 2028-01-18 ~ 2028-02-02 
+  // 2028-01-18 ~ 2028-02-02
 
   final time = DateTime(2026, 2, 10);
   final endTime = DateTime(2026, 2, 26);
@@ -22,19 +22,24 @@ final isSpringFestival = () {
 }();
 
 // 判断是否是元宵时段
-final isYuanxiaoFestival = () { 
-
-  final is2026YuanXiao = inRange(DateTime(2026, 3, 3), DateTime(2026, 3, 3)); 
+final isYuanxiaoFestival = () {
+  final is2026YuanXiao = inRange(DateTime(2027, 2, 20), DateTime(2027, 2, 20));
   final res = is2026YuanXiao;
   return res;
 }();
 
 // 判断是否是清明时段
 final isQingmingFestival = () {
-  final is2026Qingming = inRange(DateTime(2026, 4, 3), DateTime(2026, 4, 5)); 
+  final is2026Qingming = inRange(DateTime(2026, 4, 3), DateTime(2026, 4, 5));
   return is2026Qingming;
 }();
 
+// 判断是否是38时段
+final is38Festival = () {
+  final is202638 = inRange(DateTime(2026, 3, 8), DateTime(2026, 3, 8));
+  final is202738 = inRange(DateTime(2027, 3, 8), DateTime(2027, 3, 8));
+  return is202638 || is202738;
+}();
 // 获取当前的背景图
 final getBackgroundPic = () {
   var urlString = 'assets/festival/background.png';
@@ -44,6 +49,8 @@ final getBackgroundPic = () {
   } else if (isQingmingFestival) {
     //清明
     urlString = 'assets/festival/qinging.png';
+  } else if (is38Festival) {
+    urlString = 'assets/festival/38.png';
   } else {
     urlString = 'assets/festival/background.png'; //日常背景图
   }
